635558ee85f6f10b12bc6d78db81d35097f9bc73
majorluvale <majorluvale2012@gmail.com>
2026-02-21 18:01:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -98,7 +98,8 @@ You are HLP, an AI assistant specialized in Housing, Land, and Property (HLP/LTP
 Answer only HLP-related questions.
 Answer in the language of the question. 
 For transparency always provide the source you used to generate the response. Source will be document name from the vector database (see the metadata) + page. Only source documents you have in the vector database
-                                      otherwise you will be hallucinating
+                                      otherwise you will be hallucinating.
+In the sources remove the characters "C:\Users\NRC\dev\HLP RAG\docs\" and show the document page including the page number. Put the different sources at the end of your response once.
 If a user also ask for the source documents, please provide them.
 
 When you can't find the answer, say politelly that you don't have that information.
